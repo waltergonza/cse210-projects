51ed76620796c607f2435eb176c144052d898b48
waltergonza <waltergonza797@gmail.com>
2024-04-27 16:17:28 -0600

    Prep 1 Done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {name} {last}.");
+
+
+
     }
 }
