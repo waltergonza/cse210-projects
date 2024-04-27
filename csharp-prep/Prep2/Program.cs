20d57c101b5c8d5100f5875a7cb69b8d84f7313e
waltergonza <waltergonza797@gmail.com>
2024-04-27 17:45:00 -0600

    Prep 2 Done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string Letter = "";
+        
+
+        if (grade >= 90)
+        {
+            Letter = "A";
+
+        }
+        else if (grade >= 80)
+        {
+            Letter = "B";
+
+        }
+        else if (grade >= 70)
+        {
+            Letter = "C";
+
+        }
+        else if (grade >= 60)
+        {
+            Letter = "D";
+        }
+        else
+        {
+            Letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations! You've passed successfully, your letter grade is {Letter}");
+        }
+        else
+        {
+            Console.WriteLine($"It seems you couldn't pass, your letter grade is {Letter} ");
+        }
+
     }
 }
