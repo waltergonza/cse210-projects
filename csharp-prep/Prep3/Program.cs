0323d8389bef1c0dd9b3f0f6e392c3c51f41f2d4
waltergonza <waltergonza797@gmail.com>
2024-04-27 20:51:03 -0600

    Prep 3 Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        //asking for a magic number.
+        
+        // Console.Write("What is the magic number? ");
+        // string first = Console.ReadLine();
+        // int number = int.Parse(first);
+
+        //Random numbers:
+        Random randomgenerator = new Random();
+        int number = randomgenerator.Next(-1, 101);
+
+        int guess = -1;
+
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+ 
+
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess< number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }while (guess != number);
+     
     }
 }
